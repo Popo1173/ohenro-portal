bce11c7a95a48ae417b06b9b6e180a539f6e3416
Popo1173 <50015045+Popo1173@users.noreply.github.com>
2026-02-24 17:59:45 +0900

    Fix: css/jsのBrowerSyncさせる

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/images')
   eleventyConfig.addPassthroughCopy('src/assets/js')
+  eleventyConfig.addPassthroughCopy('src/assets/css')
+
+  //　BrowserSync
+  eleventyConfig.setBrowserSyncConfig({
+    files: ['./dist/assets/css/**/*.css', './dist/assets/js/**/*.js'],
+  })
+
   return {
     dir: {
       input: 'src',
